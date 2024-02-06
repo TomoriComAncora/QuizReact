502d30b5f0e59f7e556dbd8d46216ea537f9e154
TomoriComAncora <lucas_fernando@ufms.br>
2024-02-06 16:42:57 -0400

    funcionalidade de reiniciar o jogo

--- quiz/src/components/GameOver.jsx
+++ quiz/src/components/GameOver.jsx
@@ -1,19 +1,20 @@
-import { Context } from "react";
+import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-import WellDone from "../img/welldone.svg"
-import "./GameOver.css"
+import WellDone from "../img/welldone.svg";
+import "./GameOver.css";
 
 function GameOver() {
+  const [quizState, dispatch] = useContext(QuizContext);
   return (
     <div id="gameover">
-        <h2>Fim de Jogo!</h2>
-        <p>Pontuação: X</p>
-        <p>Você acertou Y de Z perguntas.</p>
-        <img src={WellDone} alt="Fim de Jogo" />
-        <button>Reiniciar</button>
+      <h2>Fim de Jogo!</h2>
+      <p>Pontuação: {quizState.score}</p>
+      <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas.</p>
+      <img src={WellDone} alt="Fim de Jogo" />
+      <button onClick={()=> dispatch({type: "NEW_GAME"})}>Reiniciar</button>
     </div>
-  )
+  );
 }
 
-export default GameOver
+export default GameOver;
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -7,6 +7,7 @@ const initialState = {
   gameStage: STAGES[0],
   questions,
   currentQuestions: 0,
+  score: 0,
 };
 
 const quizReducer = (state, action) => {
@@ -41,6 +42,10 @@ const quizReducer = (state, action) => {
         currentQuestions: nextQuestion,
         gameStage: endGame ? STAGES[2] : state.gameStage,
       };
+
+    case "NEW_GAME":
+      return initialState;
+      
     default:
       return state;
   }
